868cd100a92b80e2c1da64cb186d97f8bd8c53d4
mertgerdan <mert.gerdan@ggl.hisarschool.k12.tr>
2018-05-08 20:24:05 +0300

    Update Detector.java

--- Detector.java
+++ Detector.java
@@ -39,7 +39,7 @@ public class Detector extends Utilities{
 			// fileName = args[0];
 			
 			
-			fileName = "c:/users/mert/desktop/essay.txt"; 
+			fileName = "/Users/macbook/Desktop/paragraph1.txt"; 
 			splitParagraph(fileReader, fileName, bufferedReader);
 			System.out.println(paragraphs.size() + " paragraphs detected.");
 			System.out.println(getCombination(paragraphs.size(),2) + " possible paragraph combinations found.");
@@ -73,12 +73,40 @@ public class Detector extends Utilities{
 				}
 			}
 			
-			for (int i = 0; i < combinations.size(); i++){
+			/*for (int i = 0; i < combinations.size(); i++){ this prints the possible combinations onto the screen
 				System.out.println(combinations.get(i).getKey() + " " +combinations.get(i).getValue());
+			}*/
+			
+			for (int i = 0; i < getCombination(paragraphs.size(), 2); i++) {
+				Paragraph p1 = paragraphs.get(combinations.get(i).getKey());
+				p1.splitSentence();
+				p1.splitWords();
+				p1.setUniqueWordCount();
+				p1.setAverageWordCountPerSentence();
+				p1.setUWCPX();
+				
+				Paragraph p2 = paragraphs.get(combinations.get(i).getValue());
+				p2.splitSentence();
+				p2.splitWords();
+				p2.setUniqueWordCount();
+				p2.setAverageWordCountPerSentence();
+				p2.setUWCPX();
+				
+				if (p1.words.length < 50 || p2.words.length < 50) {
+					System.out.println("The program needs more than 50 words to have a reasonable comparison.");
+					break;
+				}
+				else {
+				System.out.println("Difference in Unique Word Count: " + Math.abs(p1.getUWC() - p2.getUWC()));
+				System.out.println("Difference in Average Words Per Sentence: " + Math.abs(p1.getWPS() - p2.getWPS()));
+				System.out.println("Difference in Unique Word Complexity: " + Math.abs(p1.getUWCPX() - p2.getUWCPX()));
+				
+				System.out.println("");
+				}
 			}
 			
 			
-			for (int i = 0; i < paragraphs.size(); i++){
+			/*for (int i = 0; i < paragraphs.size(); i++){
 				Paragraph p = paragraphs.get(i);
 				p.splitSentence();
 				p.splitWords();
@@ -90,7 +118,7 @@ public class Detector extends Utilities{
 						"Paragraph number: " + (i+1) + " has " + p.getUWC() + " unique words. On average, this paragraph had "
 								+ p.getWPS() + " words per sentence.");
 				System.out.println("Unique word complexity of paragraph number: " + (i+1) + " is " + p.getUWCPX());
-			}
+			}*/
 			
 		}
 
@@ -128,7 +156,3 @@ public class Detector extends Utilities{
 		
 
 	}
-
-
-
-
